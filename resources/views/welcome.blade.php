befc65f65878c600c5550d68ac19537e05155ef3
waldyantonioreyes <waldyantonioreyes@gmail.com>
2024-02-24 20:22:03 -0400

    sdfsdf
    sdfdasfas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
                     @endauth
                 </div>
             @endif
-            fdfddfd ddafdfasfa
+            fdfddfd ddafdfasfasdfdsfadsfb  
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
